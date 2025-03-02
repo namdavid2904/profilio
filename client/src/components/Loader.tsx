4c4144ddb9fb2e661ece3ee404385634e03b4e7e
namdavid2904 <89288986+namdavid2904@users.noreply.github.com>
2025-03-01 23:14:58 -0500

    Create simple Loader

---
+++ client/src/components/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader: React.FC = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="h-16 w-16 animate-spin rounded-full border-b-2 border-t-2 border-primary"></div>
+    </div>
+  );
+};
+
+export default Loader;
