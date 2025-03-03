42aafac3a11d58d73dbb481d07a00f72d1da9302
namdavid2904 <89288986+namdavid2904@users.noreply.github.com>
2025-03-03 01:14:55 -0500

    Add experience route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,21 +10,23 @@ const Home = lazy(() => import('./pages/Home'))
 const About = lazy(() => import('./pages/About'))
 const Projects = lazy(() => import('./pages/Projects'))
 const Contact = lazy(() => import('./pages/Contact'))
+const Experience = lazy(() => import('./pages/Experience'))
 
 const App = () => {
   return (
     <BrowserRouter>
-      <CustomCursor />
-      <Navbar />
-      <Suspense fallback={<Loader />}>
+      <CustomCursor/>
+      <Navbar/>
+      <Suspense fallback={<Loader/>}>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/experience" element={<Experience/>}/>
+          <Route path="/projects" element={<Projects/>}/>
+          <Route path="/contact" element={<Contact/>}/>
         </Routes>
       </Suspense>
-      <Footer />
+      <Footer/>
     </BrowserRouter>
   )
 }
