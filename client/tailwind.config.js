dfff4999b6dd04ebba7bc4e96c4f9724db7f1804
namdavid2904 <89288986+namdavid2904@users.noreply.github.com>
2025-03-02 23:38:09 -0500

    Config light mode colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,6 +12,11 @@ export default {
           tertiary: "#151030",
           "black-100": "#100d25",
           "black-200": "#090325",
+          light: {
+            primary: "#ffffff",
+            secondary: "#334155",
+            tertiary: "#f1f5f9",
+          }
         },
       },
     },
